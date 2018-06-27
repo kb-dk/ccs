da47357071164f7d1736b4f39ab608a2ae84b2b0
dgj <dgj@kb .dk>
2018-06-27 15:37:37 +0200

    initial commit

---
+++ src/main/java/dk/kb/ccs/Application.java
@@ -0,0 +1,33 @@
+package dk.kb.ccs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.annotation.PostConstruct;
+
+
+/**
+ * Created by dgj on 27-06-2018.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void setup(){}
+
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en"
+      xmlns:spring="http://www.springframework.org/tags"
+      xmlns:jsp="http://java.sun.com/JSP/Page"
+      xmlns="http://www.w3.org/1999/xhtml">
+
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+Hello!
+</body>
+</html>
