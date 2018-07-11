bb798f446be18d214fc227a1d1e49a2bdbaa7f5f
nkh <nkh@kb.dk>
2018-07-11 10:50:40 +0200

    Sendmail Methood

--- src/main/java/dk/kb/ccs/SendMail.java
+++ src/main/java/dk/kb/ccs/SendMail.java
@@ -4,25 +4,27 @@ package dk.kb.ccs;
  * Created by nkh on 10-07-2018.
  */
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
-import javax.activation.*;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 
 public class SendMail {
-    InputStream inputStream;
+    private String to;
+    private String from;
+    private  String host;
+    private  String subject;
 
-    public static void main(String [] args) {
-        Properties prop = new Properties();
-        String propFileName = "application.properties";
+    public SendMail(String to, String from, String host, String subject) {
+        this.to = to;
+        this.from = from;
+        this.host = host;
+        this.subject = subject;
+    }
 
-        String to = prop.getProperty("mail.to");
-        String from = prop.getProperty("mail.from");
-        String host = "post.kb.dk";
+    public void sendmail() {
         Properties properties = System.getProperties();
         properties.setProperty("mail.smtp.host", host);
         Session session = Session.getDefaultInstance(properties);
@@ -31,7 +33,7 @@ public class SendMail {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            message.setSubject("This is the my test subject!");
+            message.setSubject(subject);
             message.setText("This is my message");
             Transport.send(message);
         } catch (MessagingException mex) {
