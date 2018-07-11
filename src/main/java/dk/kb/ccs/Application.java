704478c88fe24d427d48dd25f2f58bd18d64c6d1
nkh <nkh@kb.dk>
2018-07-11 10:50:16 +0200

    Added sendmail to Application

--- src/main/java/dk/kb/ccs/Application.java
+++ src/main/java/dk/kb/ccs/Application.java
@@ -2,6 +2,7 @@ package dk.kb.ccs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -16,6 +17,15 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+    @Value("${mail.to}")
+    private String to;
+    @Value("${mail.from}")
+    private String from;
+    @Value("${mail.host}")
+    private String host;
+    @Value("${mail.subject}")
+    private String subject;
+
     private static Logger log = LoggerFactory.getLogger(Application.class);
 
     @Override
@@ -28,6 +38,9 @@ public class Application extends SpringBootServletInitializer {
     }
 
     @PostConstruct
-    public void setup(){}
+    public void setup(){
+        SendMail sendMail = new SendMail(to,from,host,subject);
+        sendMail.sendmail();
+    }
 
 }
